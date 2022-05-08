6695e525baae71911908950d40b8737c12c7e2f8
agusals <agusals2001@gmail.com>
2022-05-08 04:25:10 -0300

    ejer5

---
+++ Unidad 2/Ejercicio5/ActualizaValorVehiculoMod.py
@@ -0,0 +1,15 @@
+from PlanAhorroMod import PlanAhorro as pah
+import os
+
+class actualizaValor:
+
+    @classmethod
+    def actualizador(cls, lista):
+
+        for pa in lista:
+            pa.showvariables()
+            pa.setvalorauto(int(input("Actualizar valor vehiculo: ")))
+            os.system("CLS")
+
+        print("Valores actualizados!\n")
+        input()
---
+++ Unidad 2/Ejercicio5/valorcuotainfMod.py
@@ -0,0 +1,46 @@
+from PlanAhorroMod import PlanAhorro as pah
+import os
+
+class cuota:
+
+    @classmethod
+    def inf(cls, lista):
+        valordado = int(input("Ingrese valor: "))
+        os.system("CLS")
+        print("------Autos con valor de cuota inferiores al valor dado: \n")
+        print(" ")
+        for pa in lista:
+            valorcuota = (pa.getvalorauto()/pah.getcantcuo()) + pa.getvalorauto() * 0.1
+
+            if valorcuota < valordado:
+                pa.showvariables()
+                print(" ")
+
+        input()
+
+    
+    @classmethod
+    def montolicitar(cls, lista):
+        for pa in lista:
+            valorcuota = (pa.getvalorauto()/pah.getcantcuo()) + pa.getvalorauto() * 0.1
+            print("---Para licitar: \n")
+            pa.showvariables()
+            print("---Necesita ${}\n" .format(pa.getobjcuopagas()*valorcuota))
+            print(" ")
+        
+        input()
+
+    @classmethod
+    def modificarcuopagas(cls, lista):
+        codplandado = int(input("Ingrese Cod. Plan: "))
+        os.system("CLS")
+        e=0
+        i=0
+        while e != codplandado:
+            print("{} ".format(i))
+            if codplandado == lista[i].getcodplan():
+                lista[i].setobjcuopagas(int(input("Ingrese nueva cant. de cuotas: ")))
+                e=lista[i].getcodplan()
+            i+=1
+        os.system("CLS")
+        input("Proceso terminado, presione cualquier tecla para continuar!")
